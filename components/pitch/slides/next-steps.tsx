5a43eb7b7acd44f1b29d0eea10a46e6bfdfbe528
Julian S. <200318@studierende.htl-donaustadt.at>
2024-05-13 00:42:07 +0200

    style: add noise to bento box

--- components/pitch/slides/next-steps.tsx
+++ components/pitch/slides/next-steps.tsx
@@ -5,12 +5,12 @@ import Image from "next/image";
 import Dashboard from "@/public/images/screens/dashboard.png";
 import Images from "@/public/images/laptop-mockup.png";
 import SSSS from "@/public/images/iphone-mockup.png";
+import NoiseImage from "@/public/images/noise.webp";
 
 const SlideNextSteps = () => {
   return (
     <SlideShell title="Next Steps" className="grid-rows-2">
-      <Card className="flex flex-col justify-center space-y-5 p-5 h-full col-span-2 relative overflow-hidden">
-        <Noise />
+      <Card className="flex flex-col justify-center p-5 h-full col-span-2 relative overflow-hidden">
         <div className="ml-10 w-96">
           <h1 className="font-bold text-4xl">Preliminary study</h1>
           <span className="text-muted-foreground text-lg">
@@ -25,11 +25,13 @@ const SlideNextSteps = () => {
           alt="linear demo image"
           className="absolute -right-4 lg:-right-[10%]  filter -top-28 object-contain rounded-2xl rotate-6"
         />
+        <Noise />
       </Card>
-      <Card className="flex flex-col items-center justify-center space-y-5 p-5 h-full">
+      <Card className="flex flex-col items-center justify-center p-5 h-full relative">
+        <Noise />
         <h1 className="font-bold text-4xl">PLACEHOLDER</h1>
       </Card>
-      <Card className="flex flex-col items-center justify-center space-y-5 p-5 h-full col-span-3 relative overflow-hidden">
+      <Card className="flex flex-col items-center justify-center p-5 h-full col-span-3 relative overflow-hidden">
         <div className="w-[600px] text-center flex flex-col gap-5">
           <h1 className="font-bold text-4xl">
             Evolving User Experience through UI Implementation
@@ -53,6 +55,7 @@ const SlideNextSteps = () => {
           alt="linear demo image"
           className="absolute -right-left -left-[15%] -bottom-16 object-contain rounded-2xl"
         />
+        <Noise />
       </Card>
     </SlideShell>
   );
@@ -61,9 +64,9 @@ const SlideNextSteps = () => {
 const Noise = () => {
   return (
     <div
-      className="absolute inset-0 w-full h-full scale-[1.2] transform opacity-10 [mask-image:radial-gradient(#fff,transparent,75%)]"
+      className="absolute inset-0 w-full h-full transform opacity-[0.015] [mask-image:radial-gradient(#fff,#ffed00,10%)]"
       style={{
-        backgroundImage: "url(/noise.webp)",
+        backgroundImage: `url(${NoiseImage.src})`,
         backgroundSize: "30%",
       }}
     ></div>
