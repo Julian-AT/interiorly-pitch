3d18ca6f0d9b2f4a9ad8679f577f99a7561fd05c
Julian S. <200318@studierende.htl-donaustadt.at>
2024-05-26 00:43:01 +0200

    chore: add mobile block

--- app/[locale]/(v1)/elevator-pitch/page.tsx
+++ app/[locale]/(v1)/elevator-pitch/page.tsx
@@ -5,19 +5,24 @@ import Image from "next/image";
 export default function ElevatorPitch() {
   return (
     <div className="fixed top-0 bottom-0 right-0 left-0 h-screen bg-[#0C0C0C] text-white">
-      <Grid />
-      <PitchCarousel
-        slides={[
-          "SlideStart",
-          "SlideProblem",
-          "SlideSolution",
-          "SlideFeatures",
-          "SlideGenerativeUI",
-          "SlideVision",
-          "SlideTeam",
-          "SlideFinish",
-        ]}
-      />
+      <div className="xl:block hidden">
+        <Grid />
+        <PitchCarousel
+          slides={[
+            "SlideStart",
+            "SlideProblem",
+            "SlideSolution",
+            "SlideFeatures",
+            "SlideGenerativeUI",
+            "SlideVision",
+            "SlideTeam",
+            "SlideFinish",
+          ]}
+        />
+      </div>
+      <div className="xl:hidden flex items-center justify-center font-bold text-3xl h-full text-center">
+        We&apos;re sorry, this pitch does not work on mobile.
+      </div>
     </div>
   );
 }
--- app/[locale]/(v1)/page.tsx
+++ app/[locale]/(v1)/page.tsx
@@ -1,3 +1,4 @@
+import ImageGenerationResultCarousel from "@/components/features/image-generation-result";
 import PitchCarousel from "@/components/pitch/pitch-carousel";
 import { Grid } from "@/components/pitch/ui";
 import Image from "next/image";
@@ -5,8 +6,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="fixed top-0 bottom-0 right-0 left-0 h-screen bg-[#0C0C0C] text-white">
-      <Grid />
-      <div className="md:block hidden">
+      <div className="xl:block hidden">
+        <Grid />
         <PitchCarousel
           slides={[
             "SlideStart",
@@ -25,9 +26,10 @@ export default function Home() {
           ]}
         />
       </div>
-      <div className="md:hidden block">
+      <div className="xl:hidden flex items-center justify-center font-bold text-3xl h-full text-center">
         We&apos;re sorry, this pitch does not work on mobile.
       </div>
+      <ImageGenerationResultCarousel />
     </div>
   );
 }
