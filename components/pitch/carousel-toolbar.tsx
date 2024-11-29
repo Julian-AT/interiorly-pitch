3593fe503390156f479135b76626f8467f5df1f0
Julian S. <200318@studierende.htl-donaustadt.at>
2024-11-29 02:16:32 +0100

    refactor/feat: add theme selector and add view for mobile only

--- components/pitch/carousel-toolbar.tsx
+++ components/pitch/carousel-toolbar.tsx
@@ -12,8 +12,8 @@ import { IconChevronLeft, IconChevronRight } from "@/components/icons";
 import { cn } from "@/lib/utils";
 import LanguageSelectionDropdown from "@/components/pitch/language-dropdown";
 import { Separator } from "@/components/ui/separator";
-import InteriorlyLogoSVG from "@/public/images/logo.svg";
-import Image from "next/image";
+import Logo from "@/components/logo";
+import ThemeDropdown from "@/components/theme-dropdown";
 
 type CarouselToolbarProps = {
   api: any;
@@ -36,53 +36,61 @@ export function CarouselToolbar({ api }: CarouselToolbarProps) {
       }}
     >
       <TooltipProvider delayDuration={20}>
-        <div className="mb-8 flex h-10 items-center space-x-3 rounded-2xl border border-[#2C2C2C] px-4 py-2 backdrop-blur-lg backdrop-filter dark:bg-[#1A1A1A]/80">
-          <Image
-            src={InteriorlyLogoSVG}
-            alt="Interiorly Logo"
-            className="-mr-1.5 h-6 w-6 rounded-full"
-          />
-          <span className="relative flex gap-1 text-nowrap text-[#878787]">
-            Interiorly AI Pitch
-          </span>
-          <Separator orientation="vertical" />
-          <LanguageSelectionDropdown />
-          <Separator orientation="vertical" />
-          <div className="flex items-center">
-            <Tooltip>
-              <TooltipTrigger asChild>
-                <button
-                  type="button"
-                  disabled={!api?.canScrollPrev}
-                  className={cn(!api?.canScrollPrev && "opacity-50")}
-                  onClick={() => {
-                    api.scrollPrev();
-                  }}
+        <div className="flex h-14 w-full items-center justify-between space-x-3 border px-4 py-2 backdrop-blur-lg backdrop-filter dark:bg-[#1A1A1A]/80 md:mb-8 md:h-10 md:w-auto md:rounded-2xl md:py-2">
+          <div className="flex items-center gap-1.5 text-muted-foreground">
+            <Logo className="h-6 w-6 text-muted-foreground" />
+            <span className="relative mr-1.5 flex gap-1 text-nowrap text-sm">
+              Interiorly AI Pitch
+            </span>
+            <Separator orientation="vertical" className="hidden h-6 md:block" />
+          </div>
+          <div className="flex items-center gap-3">
+            <ThemeDropdown />
+            <Separator orientation="vertical" className="h-6" />
+            <LanguageSelectionDropdown />
+            <Separator orientation="vertical" className="h-6" />
+            <div className="flex items-center">
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <button
+                    type="button"
+                    disabled={!api?.canScrollPrev}
+                    className={cn(!api?.canScrollPrev && "opacity-50")}
+                    onClick={() => {
+                      api.scrollPrev();
+                    }}
+                  >
+                    <IconChevronLeft className="h-5 w-5 text-muted-foreground" />
+                  </button>
+                </TooltipTrigger>
+                <TooltipContent
+                  className="rounded-sm px-3 py-1"
+                  sideOffset={25}
                 >
-                  <IconChevronLeft className="h-5 w-5 text-[#878787]" />
-                </button>
-              </TooltipTrigger>
-              <TooltipContent className="rounded-sm px-3 py-1" sideOffset={25}>
-                <span className="text-xs">Vorherige Slide</span>
-              </TooltipContent>
-            </Tooltip>
-            <Tooltip>
-              <TooltipTrigger asChild>
-                <button
-                  type="button"
-                  disabled={!api?.canScrollNext}
-                  className={cn(!api?.canScrollNext && "opacity-50")}
-                  onClick={() => {
-                    api.scrollNext();
-                  }}
+                  <span className="text-xs">Vorherige Slide</span>
+                </TooltipContent>
+              </Tooltip>
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <button
+                    type="button"
+                    disabled={!api?.canScrollNext}
+                    className={cn(!api?.canScrollNext && "opacity-50")}
+                    onClick={() => {
+                      api.scrollNext();
+                    }}
+                  >
+                    <IconChevronRight className="h-5 w-5 text-muted-foreground" />
+                  </button>
+                </TooltipTrigger>
+                <TooltipContent
+                  className="rounded-sm px-3 py-1"
+                  sideOffset={25}
                 >
-                  <IconChevronRight className="h-5 w-5 text-[#878787]" />
-                </button>
-              </TooltipTrigger>
-              <TooltipContent className="rounded-sm px-3 py-1" sideOffset={25}>
-                <span className="text-xs">Nächste slide</span>
-              </TooltipContent>
-            </Tooltip>
+                  <span className="text-xs">Nächste slide</span>
+                </TooltipContent>
+              </Tooltip>
+            </div>
           </div>
         </div>
       </TooltipProvider>
