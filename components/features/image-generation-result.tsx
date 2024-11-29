cc3344f35db23cdca2796ec53cdc54f9be5f5f7b
Julian S. <200318@studierende.htl-donaustadt.at>
2024-11-29 02:17:08 +0100

    feat: add image preview dialog on click

--- components/features/image-generation-result.tsx
+++ components/features/image-generation-result.tsx
@@ -17,13 +17,17 @@ import { useImageGeneration } from "@/lib/hooks/use-images";
 import { useEffect, useState } from "react";
 import { Button } from "@/components/ui/button";
 import { IconSparkles } from "@/components/icons";
-import Logo from "../logo";
-import { DialogTitle } from "@radix-ui/react-dialog";
+import Logo from "@/components/logo";
+import { DialogTitle } from "@/components/ui/dialog";
+import { ArrowLeft, ArrowRight } from "lucide-react";
 
 export default function ImageGenerationResultCarousel() {
   const { images, progress, message } = useImageGeneration();
   const [api, setApi] = useState<CarouselApi>();
   const [current, setCurrent] = useState(0);
+  const [selectedImageIndex, setSelectedImageIndex] = useState<number | null>(
+    null,
+  );
 
   useEffect(() => {
     if (!api) {
@@ -56,68 +60,101 @@ export default function ImageGenerationResultCarousel() {
             </span>
           </div>
         ) : (
-          <Carousel setApi={setApi}>
-            <CarouselContent>
-              {images[0].batch.map((image, index) => (
-                <CarouselItem key={index}>
-                  <Dialog>
-                    <DialogTrigger asChild>
-                      <div className="cursor-pointer">
-                        <Image
-                          src={image}
-                          alt="Generated Image"
-                          width="0"
-                          height="0"
-                          className="aspect-square w-full rounded-lg shadow-2xl"
-                        />
-                        <div className="relative">
-                          <div className="absolute bottom-1 right-2 opacity-50">
-                            <Logo className="h-10 w-10 text-white" />
-                          </div>
+          <>
+            <Carousel setApi={setApi}>
+              <CarouselContent>
+                {images[0].batch.map((image, index) => (
+                  <CarouselItem key={index}>
+                    <div
+                      className="cursor-pointer"
+                      onClick={() => setSelectedImageIndex(index)}
+                    >
+                      <Image
+                        src={image}
+                        alt="Generated Image"
+                        width="0"
+                        height="0"
+                        className="aspect-square w-full rounded-lg shadow-2xl"
+                      />
+                      <div className="relative">
+                        <div className="absolute bottom-1 right-2 opacity-50">
+                          <Logo className="h-10 w-10 text-white" />
                         </div>
                       </div>
-                    </DialogTrigger>
-                    <DialogContent className="h-[95%] max-w-screen-lg border border-green-500">
-                      <DialogTitle>
-                        <span className="text-lg font-bold">
-                          {images[0].prompt}
-                        </span>
-                      </DialogTitle>
-                      <div className="relative h-full w-full p-12">
-                        <Image
-                          src={image}
-                          alt="Generated Image"
-                          width={1024}
-                          height={1024}
-                          className="relative h-full w-full overflow-hidden rounded-lg object-contain"
-                        />
-                        <CarouselPrevious className="h-12 w-12 border border-red-500 p-1.5" />
-                        <CarouselNext className="h-12 w-12 border border-red-500 p-1.5" />
-                      </div>
-                    </DialogContent>
-                  </Dialog>
-                </CarouselItem>
-              ))}
-            </CarouselContent>{" "}
-            <CarouselPrevious />
-            <CarouselNext />
-            <div className="flex justify-center gap-2 p-4">
-              {Array.from({ length: images[0].batch.length }).map(
-                (_, index) => (
-                  <Button
-                    variant="default"
-                    key={index}
-                    aria-label={`Go to slide ${index + 1}`}
-                    onClick={() => api?.scrollTo(index)}
-                    className={cn(
-                      "h-2 w-2 rounded-full p-0",
-                      index + 1 === current ? "bg-primary" : "bg-secondary",
-                    )}
-                  />
-                ),
+                    </div>
+                  </CarouselItem>
+                ))}
+              </CarouselContent>
+              <CarouselPrevious />
+              <CarouselNext />
+              <div className="flex justify-center gap-2 p-4">
+                {Array.from({ length: images[0].batch.length }).map(
+                  (_, index) => (
+                    <Button
+                      variant="default"
+                      key={index}
+                      aria-label={`Go to slide ${index + 1}`}
+                      onClick={() => api?.scrollTo(index)}
+                      className={cn(
+                        "h-2 w-2 rounded-full p-0",
+                        index + 1 === current ? "bg-primary" : "bg-secondary",
+                      )}
+                    />
+                  ),
+                )}
+              </div>
+            </Carousel>
+
+            <Dialog
+              open={selectedImageIndex !== null}
+              onOpenChange={() => setSelectedImageIndex(null)}
+            >
+              {selectedImageIndex !== null && (
+                <DialogContent className="h-[95vh] max-w-screen-lg select-none overflow-hidden">
+                  <DialogTitle className="text-lg font-bold">
+                    {images[0].prompt}
+                  </DialogTitle>
+                  <div className="relative h-full w-full overflow-hidden">
+                    <Image
+                      src={images[0].batch[selectedImageIndex]}
+                      alt="Generated Image"
+                      width={1024}
+                      height={1024}
+                      className="relative h-full w-full overflow-hidden rounded-lg object-contain"
+                    />
+                    <Button
+                      variant="outline"
+                      className="absolute left-4 top-1/2 h-12 w-12 -translate-y-1/2 rounded-full"
+                      onClick={() =>
+                        setSelectedImageIndex((prev) =>
+                          prev !== null && prev > 0 ? prev - 1 : prev,
+                        )
+                      }
+                      disabled={selectedImageIndex === 0}
+                    >
+                      <ArrowLeft className="h-full w-full" />
+                    </Button>
+                    <Button
+                      variant="outline"
+                      className="absolute right-4 top-1/2 h-12 w-12 -translate-y-1/2 rounded-full"
+                      onClick={() =>
+                        setSelectedImageIndex((prev) =>
+                          prev !== null && prev < images[0].batch.length - 1
+                            ? prev + 1
+                            : prev,
+                        )
+                      }
+                      disabled={
+                        selectedImageIndex === images[0].batch.length - 1
+                      }
+                    >
+                      <ArrowRight className="h-full w-full" />
+                    </Button>
+                  </div>
+                </DialogContent>
               )}
-            </div>
-          </Carousel>
+            </Dialog>
+          </>
         )
       ) : (
         <div className="flex w-full flex-col items-center justify-center gap-2 text-center">
